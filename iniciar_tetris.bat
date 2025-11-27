c0d262cd40fa02d5037b30992be27edbd45beab2
Damian Torres <damian.torrescadena0563@upse.edu.ec>
2025-11-27 11:09:06 -0500

    Update iniciar_tetris.bat

--- iniciar_tetris.bat
+++ iniciar_tetris.bat
@@ -3,15 +3,23 @@ title Instalando y ejecutando Tetris
 echo =========================================
 echo     Verificando si Python está instalado
 echo =========================================
+
 python --version >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
     echo.
     echo Python no está instalado.
     echo Descargando instalador de Python...
-    curl https://www.python.org/ftp/python/3.11.9/python-3.11.9-amd64.exe -o python_installer.exe
+
+    powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.11.9/python-3.11.9-amd64.exe' -OutFile 'python_installer.exe'"
+
     echo Instalando Python...
     start /wait python_installer.exe /quiet InstallAllUsers=1 PrependPath=1
     del python_installer.exe
+
+    echo.
+    echo Python instalado correctamente.
+) ELSE (
+    echo Python ya está instalado.
 )
 
 echo.
